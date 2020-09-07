185d372abbf98231a787694765b8505f89aea588
Mr.Engineer Biruk <meetbirukberhanu@gmail.com>
2020-09-07 15:56:38 +0300

    preparing for heroku development

--- restart.js
+++ restart.js
@@ -1,18 +1,22 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require('fs')
+const path = require('path')
 
-let news = path.join(__dirname, "data", "NEWS.json")
-let newsData = JSON.stringify([])
-fs.writeFileSync(news, newsData, "utf-8")
+let restart = function () {
+	let news = path.join(__dirname, 'data', 'NEWS.json')
+	let newsData = JSON.stringify([])
+	fs.writeFileSync(news, newsData, 'utf-8')
 
-let latest = path.join(__dirname, "data", "latest.json")
-let latestData = JSON.stringify([
-	{ website: "netflix", latestTitle: " bla bla" },
-	{ website: "addisFortune", latestTitle: " bla bla" },
-	{ website: "newBusinessEthiopia", latestTitle: " bla bla" },
-	{ website: "thereportermagazines", latestTitle: " bla bla" },
-	{ website: "2merkato", latestTitle: " bla bla" },
-	{ website: "ethiopianmonitor", latestTitle: " bla bla" },
-	{ website: "furtherafrica", latestTitle: " bla bla" },
-])
-fs.writeFileSync(latest, latestData, "utf-8")
+	let latest = path.join(__dirname, 'data', 'latest.json')
+	let latestData = JSON.stringify([
+		{ website: 'netflix', latestTitle: ' bla bla' },
+		{ website: 'addisFortune', latestTitle: ' bla bla' },
+		{ website: 'newBusinessEthiopia', latestTitle: ' bla bla' },
+		{ website: 'thereportermagazines', latestTitle: ' bla bla' },
+		{ website: '2merkato', latestTitle: ' bla bla' },
+		{ website: 'ethiopianmonitor', latestTitle: ' bla bla' },
+		{ website: 'furtherafrica', latestTitle: ' bla bla' },
+	])
+	fs.writeFileSync(latest, latestData, 'utf-8')
+}
+
+module.exports = restart
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config({
 })
 const { bot } = require('./bot')
 
-const moneyExchange = require('./RSSWebsites/EthiopianMoneyExchange/ETBExg')
 const netflix = require('./RSSWebsites/netflix/netflix')
 const thereportermagazines = require('./RSSWebsites/businessEnglish/thereportermagazines/thereportermagazines')
 const addisFortune = require('./RSSWebsites/businessEnglish/addisFortune/addisFortune')
@@ -11,8 +10,88 @@ const newBusinessEthiopia = require('./RSSWebsites/businessEnglish/newBusinessEt
 const furtherafrica = require('./RSSWebsites/businessEnglish/furtherafrica/furtherafrica')
 const ethiopianmonitor = require('./RSSWebsites/businessEnglish/ethiopianmonitor/ethiopianmonitor')
 const merkato2 = require('./RSSWebsites/businessEnglish/2merkato/2merkato')
+const moneyExchange = require('./RSSWebsites/EthiopianMoneyExchange/ETBExg')
 
-let fiveSec = 5 * 1000
+if (process.env.NODE_ENV == 'production') {
+	netflix.fetchAndPost()
+	merkato2.fetchAndPost()
+
+	let fiveMinute = 1000 * 60 * 5
+	let tenMinute = 1000 * 60 * 10
+	let fifteenMinute = 1000 * 60 * 15
+	let twentyFiveMinute = 1000 * 60 * 25
+
+	let oneHour = 1000 * 60 * 60 * 1
+	let twoHour = oneHour * 2
+	let threeHour = oneHour * 3
+	let fourHour = two * 2
+	let fiveHour = oneHour * 5
+	let sixHour = threeHour * 2
+	let twelveHour = sixHour * 2
+
+	function oneHourFunction() {
+		netflix.fetchAndPost()
+		thereportermagazines.fetchAndPost()
+	}
+	setInterval(oneHourFunction, oneHour + twentyFiveMinute)
+
+	function twoHourFunction() {
+		addisFortune.fetchAndPost()
+	}
+	setInterval(twoHourFunction, twoHour + tenMinute)
+
+	function threeHourFunction() {
+		ethiopianmonitor.fetchAndPost()
+	}
+	setInterval(threeHourFunction, threeHour + fiveMinute)
+
+	function fourHourFunction() {
+		furtherafrica.fetchAndPost()
+	}
+	setInterval(fourHourFunction, fourHour + twentyFiveMinute)
+
+	function fiveHourFunction() {
+		newBusinessEthiopia.fetchAndPost()
+	}
+	setInterval(fiveHourFunction, fiveHour + tenMinute)
+
+	function sixHourFunction() {
+		merkato2.fetchAndPost()
+	}
+	setInterval(sixHourFunction, sixHour + fifteenMinute)
+
+	function twelveHourFunction() {
+		moneyExchange.fetchAndPost()
+	}
+	setInterval(sixHourFunction, twelveHour + fifteenMinute)
+}
+
+// if (process.env.NODE_ENV === "development") {
+// 	console.log("development In")
+
+// 	const oneMinute = 400 * 70 * 1
+// 	const twoMinute = 400 * 60 * 2
+// 	const fourMinute = 400 * 50 * 4
+
+// 	function oneMinuteFunction() {
+// 		merkato2.fetchAndPost()
+// 	}
+// 	setInterval(oneMinuteFunction, oneMinute)
+
+// 	function twoMinuteFunction() {
+// 		addisFortune.fetchAndPost()
+// 		newBusinessEthiopia.fetchAndPost()
+// 	}
+// 	setInterval(twoMinuteFunction, twoMinute)
+
+// 	function fourMinuteFunction() {
+// 		netflix.fetchAndPost()
+// 		thereportermagazines.fetchAndPost()
+// 	}
+// 	setInterval(fourMinuteFunction, fourMinute)
+// }
+
+let tenSec = 10 * 1000
 
 bot.command('part1', ctx => {
 	setTimeout(() => {
@@ -21,9 +100,9 @@ bot.command('part1', ctx => {
 			thereportermagazines.fetchAndPost()
 			setTimeout(() => {
 				newBusinessEthiopia.fetchAndPost()
-			}, fiveSec)
-		}, fiveSec)
-	}, fiveSec)
+			}, tenSec)
+		}, tenSec)
+	}, tenSec)
 })
 bot.command('part2', ctx => {
 	setTimeout(() => {
@@ -31,30 +110,20 @@ bot.command('part2', ctx => {
 		setTimeout(() => {
 			setTimeout(() => {
 				ethiopianmonitor.fetchAndPost()
-			}, fiveSec)
-		}, fiveSec)
-	}, fiveSec)
+			}, tenSec)
+		}, tenSec)
+	}, tenSec)
 })
 bot.command('part3', ctx => {
 	setTimeout(() => {
 		merkato2.fetchAndPost()
 		setTimeout(() => {
 			netflix.fetchAndPost()
-		}, fiveSec)
-	}, fiveSec)
+		}, tenSec)
+	}, tenSec)
 })
 bot.command('money', ctx => {
 	moneyExchange.fetchAndPost()
 })
 
-// bot.launch()
-
-exports.handler = (event, context, callback) => {
-	const tmp = JSON.parse(event.body) // get data passed to us
-	bot.handleUpdate(tmp) // make Telegraf process that data
-	return callback(null, {
-		// return something for webhook, so it doesn't try to send same stuff again
-		statusCode: 200,
-		body: '',
-	})
-}
+bot.launch()
